a53a4efe5b53f26196309a37ba787990840ae645
reda.baalou@uit.ac.ma <reda.baalou.dev@gmail.com>
2022-11-25 20:49:46 +0100

    creation des contract et du xsd et genration des class java

---
+++ src/main/java/com/crudsoap/soapapplication/SoapApplication.java
@@ -0,0 +1,13 @@
+package com.crudsoap.soapapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoapApplication.class, args);
+    }
+
+}
